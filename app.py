d4b05af29e5c4df8ea8f1040803d35db62fdbcd6
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-06-11 20:05:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,44 +35,4 @@ result = streamlit_bokeh_events(
 if result:
     if "GET_TEXT" in result:
         st.write(result.get("GET_TEXT"))
-        
-from deepgram import Deepgram
-import asyncio
-import aiohttp
-
-DEEPGRAM_API_KEY = 'd49524b7dd14cfbe87b6f794b7b87e6f38102cb1'
-URL = 
-async def main():
-  deepgram = Deepgram(DEEPGRAM_API_KEY)
-  try:
-    deepgramLive = await deepgram.transcription.live({
-      'punctuate': True,
-      'interim_results': False,
-      'language': 'uk'
-    })
-  except Exception as e:
-    print(f'Could not open socket: {e}')
-    return
-
-  deepgramLive.registerHandler(deepgramLive.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
-
-  # Listen for any transcripts received from Deepgram and write them to the console
-  deepgramLive.registerHandler(deepgramLive.event.TRANSCRIPT_RECEIVED, print)
-
-  # Listen for the connection to open and send streaming audio from the URL to Deepgram
-  async with aiohttp.ClientSession() as session:
-    async with session.get(URL) as audio:
-      while True:
-       data = await audio.content.readany()
-       deepgramLive.send(data)
-
-       # If no data is being sent from the live stream, then break out of the loop.
-       if not data:
-         break
-
-  # Indicate that we've finished sending data by sending the customary zero-byte message to the Deepgram streaming endpoint, and wait until we get back the final summary metadata object
-  await deepgramLive.finish()
-
-# If running in a Jupyter notebook, Jupyter is already running an event loop, so run main with this line instead:
-#await main()
-asyncio.run(main())
+ 
