0f503d4caa426a6a42c96783f4b08671fd86cd1e
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-06-11 20:31:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
 import streamlit as st
-from IPython.display import Audio
-from ipywebrtc import CameraStream, AudioRecorder
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-# actually I found this hack in some js code
-# just pass mime type =)
-camera = CameraStream(constraints={'audio': True,
-                                   'video': False},
-                      mimeType='audio/wav')
-recorder = AudioRecorder(stream=camera)
+stt_button = Button(label="Speak", width=100)
 
-# turn the recorder on
-# still a bit rusty on whether I need to show it 
-# in a separate cell later to make it work
-if recorder.recording:
-  recorder.save('test.wav')
-# say something
-# turn the recorder off
-#recorder.recording = False
-
-# enjoy your wav (a typical user will be happy with compressed sound ofc)
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = false;
+    recognition.interimResults = true;
  
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
