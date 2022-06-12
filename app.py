345762f790cc73fcc44dc2132dd08073e2367c44
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-06-12 10:58:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import streamlit as st
-import requests
-r = requests.get('https://api.ipdata.co?api-key=88e9d071ff5d290a3400ffb486184750e54f7f50affb3c8dc750878f').json()
-clicked = st.button("Give me your location")
-if clicked:
-    st.write(r)
-ipdata me
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+
+loc_button = Button(label="Get Location")
+loc_button.js_on_event("button_click", CustomJS(code="""
+    navigator.geolocation.getCurrentPosition(
+        (loc) => {
+            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+        }
+    )
+    """))
+result = streamlit_bokeh_events(
+    loc_button,
+    events="GET_LOCATION",
+    key="get_location",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
