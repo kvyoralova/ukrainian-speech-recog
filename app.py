1a5b9af0e0266cd78566faba347dd42ba498625e
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-06-12 13:26:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ import leafmap.foliumap as leafmap
 import geopy
 from geopy.geocoders import Nominatim
 
-
-def app():
-
-    loc_button = Button(label="Get Device Location", max_width=150)
+def geo():
+    loc_button = Button(label="Отримати місцезнаходження пристрою", max_width=250)
     loc_button.js_on_event(
         "button_click",
         CustomJS(
@@ -31,22 +29,27 @@ def app():
         override_height=75,
         debounce_time=0,
     )
-
     if result:
         if "GET_LOCATION" in result:
             loc = result.get("GET_LOCATION")
             lat = loc.get("lat")
             lon = loc.get("lon")
-            st.write(f"Lat, Lon: {lat}, {lon}")
+            #st.write(f"Lat, Lon: {lat}, {lon}")
             latlon = str(lat) + ", " + str(lon)
             geoLoc = Nominatim(user_agent="GetLoc")
             locname = geoLoc.reverse(latlon)
-            st.write(locname.address)
+            address = locname.address
+            import re
+            regexpr = r'[A-Za-z]+'
+            actual_location = re.findall(regexpr, address.lower())
+            country = actual_location[-1]
+            if country == "italia":
+              language = "it"
+              st.write("Ви перебуваєте в Італії, тому програма працюватиме італійською мовою.")
+              st.write("Sei in Italia, quindi l'app verrà eseguita in italiano.")
+            else:
+              language =  "en"
+              st.write("Ви перебуваєте за межами Італії, тому програма працюватиме англійською мовою.")
+              st.write("You are out of Italy, so the app will run in English.")
 
-            m = leafmap.Map(center=(lat, lon), zoom=16)
-            m.add_basemap("ROADMAP")
-            popup = f"lat, lon: {lat}, {lon}"
-            m.add_marker(location=(lat, lon), popup=popup)
-            m.to_streamlit()
-
-app()
+geo()
