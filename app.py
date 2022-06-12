22573586ac8189c3217dbdedcd28d111ad499278
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-06-12 15:07:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,19 @@
 import time
 import streamlit as st
 
-placeholder = st.empty()
+#placeholder = st.empty()
 
 # Replace the placeholder with some text:
-placeholder.text("Hello")
+#placeholder.text("Hello")
 
 # Replace the text with a chart:
-placeholder.line_chart({"data": [1, 5, 2, 6]})
+#placeholder.line_chart({"data": [1, 5, 2, 6]})
 
 # Replace the chart with several elements:
 with placeholder.container():
      st.write("This is one element")
      st.write("This is another")
+     time.sleep(5)
 
 # Clear all those elements:
 placeholder.empty()
