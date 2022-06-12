afbbd51093dea91af6483ea0013553fc484462ff
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-06-12 11:11:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 import leafmap.foliumap as leafmap
+from geopy.geocoders import Nominatim
 
 
 def app():
@@ -36,6 +37,10 @@ def app():
             lat = loc.get("lat")
             lon = loc.get("lon")
             st.write(f"Lat, Lon: {lat}, {lon}")
+            
+            geoLoc = Nominatim(user_agent="GetLoc")
+            locname = geoLoc.reverse(lat, lon)
+            st.write(locname.address)
 
             m = leafmap.Map(center=(lat, lon), zoom=16)
             m.add_basemap("ROADMAP")
@@ -43,3 +48,4 @@ def app():
             m.add_marker(location=(lat, lon), popup=popup)
             m.to_streamlit()
 
+app()
