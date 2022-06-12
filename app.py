79b8729d147a7e3afd5da7cb9ea69f6e374c8a7f
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-06-12 10:49:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import requests
 r = requests.get('https://api.ipdata.co?api-key=88e9d071ff5d290a3400ffb486184750e54f7f50affb3c8dc750878f').json()
 clicked = st.button("Give me your location")
