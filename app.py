cfc624209a48c41ce53fac8e4637efebf38de788
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-06-12 10:49:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import requests
 r = requests.get('https://api.ipdata.co?api-key=88e9d071ff5d290a3400ffb486184750e54f7f50affb3c8dc750878f').json()
 clicked = st.button("Give me your location")
 if clicked:
-    pprint(r)
+    st.write(r)
